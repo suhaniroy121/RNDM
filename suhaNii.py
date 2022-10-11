1b33138d9638c3489b85dd999a3a5fb9725a28d4
suhaniroy121 <115403193+suhaniroy121@users.noreply.github.com>
2022-10-11 20:31:28 +0530

    Update suhaNii.py

--- suhaNii.py
+++ suhaNii.py
@@ -1,11 +1,44 @@
-import os,time
 
-os.system("xdg-open https://youtube.com/channel/UCbT--Z1XzQpSUgjD6bfzzUA")
 
-print("\n\n\t\033[1;32m")
 
-time.sleep(1)
 
-import SUHANII
 
-SUHANII.ud()
+
+
+
+
+
+
+
+
+import os, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('xdg-open https://youtube.com/channel/UCbT--Z1XzQpSUgjD6bfzzUA')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from SUHANII import ud
+
+    ud()
+
+elif bit == '32bit':
+
+    from SUHANII32 import ud
+
+    ud()
+
+else:
+
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
+
+    os.system('exit')
